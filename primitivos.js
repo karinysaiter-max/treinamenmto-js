a79533895aa59813fffacd8bccf71297464316fe
kariny <kariny.saiter@escola.pr.gov.br>
2026-03-19 15:51:03 -0300

    criei o arquivo primitivos.js

---
+++ primitivos.js
@@ -0,0 +1,37 @@
+// criar variavel
+let nextstage = "valor variavel";
+var omori = "valor variavel de escopo diferente";
+const stardewValle = "valor absoluto";
+
+// o que não fazer
+// let NOME
+// let 1nome
+// let Nome
+
+// tipo number
+let numero = 5;
+const pi = 3.14;
+
+// tipo string e tamplete
+let roblox = 'quero injustiça';
+let minecraft = "meu amigo não sabe construir casa direito";
+let romi = `${minecraft},${roblox}!!`;
+
+// tipo booleano
+let gta6 = false;
+let gta5 = true;
+
+// tipo object
+let gojo = {
+    cla : "gojo",
+    nome : "Satoru Gojo",
+    idade : 29,
+    provaVida : false,
+    familia : null,
+    ensinamento : "não substime os outros"
+
+};
+
+// mostrar variaveis no terminal 
+console.log(romi);
+console.log (gojo.ensinamento)
